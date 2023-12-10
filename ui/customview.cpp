167325f30b56d3e8b2a50bcb228d9ebfab8f3e80
Termtre <fomichiev_2013@mail.ru>
2023-12-10 23:46:57 +0300

    Интерфейс

---
+++ ui/customview.cpp
@@ -0,0 +1,79 @@
+#include "customview.h"
+
+CustomView::CustomView(QWidget* parent) : QChartView(parent)
+{
+
+}
+
+void CustomView::zoomIt(bool zoom)
+{
+    itZoom = zoom;
+}
+
+void CustomView::wheelEvent(QWheelEvent *event)
+{
+    if (itZoom)
+    {
+        QPointF cur = this->chart()->mapToValue(event->position());
+        QValueAxis *axisX = (QValueAxis*)this->chart()->axes(Qt::Horizontal).at(0);
+        QValueAxis *axisY = (QValueAxis*)this->chart()->axes(Qt::Vertical).at(0);
+        double MinX = axisX->min();
+        double MaxX = axisX->max();
+        double MinY = axisY->min();
+        double MaxY = axisY->max();
+
+        double ZoomMinX, ZoomMaxX, ZoomMinY, ZoomMaxY;
+        double koef;
+#ifdef __APPLE__
+        koef = 1.1;
+#else
+        koef = 1.3;
+#endif
+
+        if(event->angleDelta().y() > 0)
+        {
+            ZoomMinX = cur.x() - (cur.x() - MinX) / koef;
+            ZoomMaxX = cur.x() - (cur.x() - MaxX) / koef;
+            ZoomMinY = cur.y() - (cur.y() - MinY) / koef;
+            ZoomMaxY = cur.y() - (cur.y() - MaxY) / koef;
+        }
+        else
+        {
+            ZoomMinX = cur.x() - (cur.x() - MinX) * koef;
+            ZoomMaxX = cur.x() - (cur.x() - MaxX) * koef;
+            ZoomMinY = cur.y() - (cur.y() - MinY) * koef;
+            ZoomMaxY = cur.y() - (cur.y() - MaxY) * koef;
+        }
+
+        //if (ZoomMinX < -1.) ZoomMinX = -1.;
+        //if (ZoomMaxX > 2.) ZoomMaxX = 2.;
+
+        axisX->setRange(ZoomMinX, ZoomMaxX);
+        axisY->setRange(ZoomMinY, ZoomMaxY);
+    }
+}
+
+void CustomView::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        oldPoint = event->pos();
+        isPress = true;
+    }
+}
+
+void CustomView::mouseMoveEvent(QMouseEvent *event)
+{
+    if(isPress)
+    {
+        QPoint pointDiff = event->pos() - oldPoint;
+        this->chart()->scroll(-pointDiff.x(), pointDiff.y());
+    }
+
+    oldPoint = event->pos();
+}
+
+void CustomView::mouseReleaseEvent(QMouseEvent *event)
+{
+    isPress = false;
+}
---
+++ ui/mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtCharts/QtCharts>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_firstStart_clicked();
+
+    void on_secondStart_clicked();
+
+    void on_firstDel_clicked();
+
+    void on_secondDel_clicked();
+
+    void on_firstShowDot_clicked(bool checked);
+
+    void on_secondShowDot_clicked(bool checked);
+
+private:
+    QChart* chart1;
+    QChart* chart2;
+
+    QLineSeries* testAnSeries = nullptr;
+    QLineSeries* testSeries = nullptr;
+    QLineSeries* mainSeries = nullptr;
+
+    QValueAxis* axisX1;
+    QValueAxis* axisY1;
+    QValueAxis* axisX2;
+    QValueAxis* axisY2;
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
